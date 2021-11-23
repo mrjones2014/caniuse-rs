e782a1ed7f322bcd0e8d8271f944777b48f89c0d
Mat Jones <mat@mjones.network>
2021-11-23 10:55:13 -0500

    reorder lines

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,14 @@ mod browser;
 mod feature;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    api::ensure_cached_data()?;
-    let features = api::get_data()?;
     let args: Vec<String> = env::args().collect::<Vec<String>>();
     if args.len() < 2 || args[1].is_empty() {
         panic!("No query passed as argument.");
     }
 
+    api::ensure_cached_data()?;
     let query = &args[1];
+    let features = api::get_data()?;
     let filtered_features = api::fuzzy_find(&query, &features);
     println!("{}", serde_json::to_string(&filtered_features)?);
     Ok(())
