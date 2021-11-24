cc9f33e2eb80826c90911ef797d9a43079a7229a
Mat Jones <mat@mjones.network>
2021-11-24 13:32:48 -0500

    add --dump option

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate lazy_static;
 
-use std::error::Error;
+use std::{env, error::Error};
 
 mod api;
 mod browser;
@@ -12,6 +12,17 @@ mod url;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let features = api::get_data()?;
+    let args: Vec<String> = env::args().map(|arg| arg.to_lowercase()).collect();
+    if args.contains(&String::from("--dump")) {
+        let printer = if args.contains(&String::from("--pretty")) {
+            serde_json::to_string_pretty
+        } else {
+            serde_json::to_string
+        };
+        println!("{}", printer(&features)?);
+        return Ok(());
+    }
+
     skim_finder::find_with_skim(&features);
     Ok(())
 }
